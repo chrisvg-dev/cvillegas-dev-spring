654999ecee30e2273bfcc3a93fb6b5b7810e8a12
Cristhian Villegas Garcia <cristianvg9692@gmail.com>
2023-04-05 14:51:56 -0600

    Spring script for docker validation

---
+++ src/main/java/com/cvillegas/app/MainApp/model/Project.java
@@ -0,0 +1,22 @@
+package com.cvillegas.app.MainApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private String title;
+    private String username;
+    private String component;
+}
