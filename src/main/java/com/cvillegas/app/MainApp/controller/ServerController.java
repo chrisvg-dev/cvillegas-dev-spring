e0f64080c698ef1205de85e8248b8611bb09a012
Cristhian Villegas Garcia <cristianvg9692@gmail.com>
2023-04-04 22:57:12 -0600

    first commit

---
+++ src/main/java/com/cvillegas/app/MainApp/controller/ServerController.java
@@ -0,0 +1,21 @@
+package com.cvillegas.app.MainApp.controller;
+
+import com.cvillegas.app.MainApp.dto.BasicResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.cvillegas.app.MainApp.utils.Constants.*;
+
+@RestController
+@RequestMapping("/api/v1/server")
+public class ServerController {
+
+    @GetMapping
+    public ResponseEntity<?> index() {
+        BasicResponseDto basicResponse = BasicResponseDto.builder()
+                .code(OK_STATUS).message("Server is working...").build();
+        return ResponseEntity.ok(basicResponse);
+    }
+}
---
+++ src/main/java/com/cvillegas/app/MainApp/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.cvillegas.app.MainApp.utils;
+
+public class Constants {
+    public static final Integer OK_STATUS = 200;
+}
