883a951743d926f87ecad9e7c4c367cfaf80470a
Cristhian Villegas Garcia <cristianvg9692@gmail.com>
2023-10-23 00:13:31 -0600

    csc

--- src/main/java/com/cvillegas/app/main/controller/EmailSenderController.java
+++ src/main/java/com/cvillegas/app/main/controller/EmailSenderController.java
@@ -7,6 +7,8 @@ import com.cvillegas.app.main.service.IEmailService;
 import com.cvillegas.app.main.utils.IPIdentifierHelper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,20 +19,13 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/info")
-@Slf4j
 @AllArgsConstructor
 public class EmailSenderController {
+    private static Logger log = LoggerFactory.getLogger(EmailSenderController.class);
     private IEmailService emailService;
 
     @PostMapping("/sendBasicEmail")
     public BasicResponseDto sendBasicEmail(HttpServletRequest servletRequest, @RequestBody BaseEmailContentDto emailContent){
-        String clientIp = servletRequest.getRemoteAddr();
-        log.info(clientIp);
-        return emailService.sendSimpleMessage(emailContent);
-    }
-
-    @PostMapping("/sendEmail")
-    public BasicResponseDto sendEmail(HttpServletRequest servletRequest, @RequestBody BaseEmailContentDto emailContent){
         String clientIp = servletRequest.getRemoteAddr();
         log.info(clientIp);
         return emailService.sendSimpleMessage(emailContent);
