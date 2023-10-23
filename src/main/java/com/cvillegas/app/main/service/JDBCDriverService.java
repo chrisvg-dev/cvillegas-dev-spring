8fd1a9b7ea3150b0f8b72b1f305439d0b7633d67
Cristhian Villegas Garcia <cristianvg9692@gmail.com>
2023-10-23 00:17:43 -0600

    csadcdsa

--- src/main/java/com/cvillegas/app/main/service/JDBCDriverService.java
+++ src/main/java/com/cvillegas/app/main/service/JDBCDriverService.java
@@ -1,2 +1,11 @@
-package com.cvillegas.app.main.service;public class JDBCDriverService {
+package com.cvillegas.app.main.service;
+
+public class JDBCDriverService {
+
+    // CREATE CONSTANTS FOR DB CONNECTION
+    private static final String DB_DRIVER = "org.postgresql.Driver";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DB_USERNAME = "postgres";
+
+
 }
--- src/test/java/com/cvillegas/app/main/repository/UserRepositoryTest.java
+++ src/test/java/com/cvillegas/app/main/repository/UserRepositoryTest.java
@@ -1,14 +1,17 @@
 package com.cvillegas.app.main.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 public class UserRepositoryTest {
+
     @Test
     public void getAllUsersTest() {
         Assertions.assertTrue(1 == 1);
+        log.info("Printed");
     }
-
 }
